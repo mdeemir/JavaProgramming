412ed722efa34012a316ec35d7c93a094a02cae7
mdeemir <95213547+mdeemir@users.noreply.github.com>
2022-04-15 05:25:57 +0300

    day 39 start

---
+++ src/day39_Recap/MethodOverloading.java
@@ -0,0 +1,20 @@
+package day39_Recap;
+
+public class MethodOverloading {
+
+/* override göre
+parameter farklı olmalı (override da aynı olacak)
+sınıf içinde oluşturulur (is a ilişkisi olan iki sınıf içinde kullanılabilir)
+access modifier değişebilir (aynısı veya daha görünür bir method olmalı)
+private and final methods overload edilebilir (private ve final ile override yapılamaz)
+return type farklı olabilir (aynı olmalı over ride da )
+ */
+    private void method1(){
+
+    }
+
+    private int method1(int a){
+        return a;
+    }
+
+}
