359656de6996adffd7a5cc5fbb6a192522f26343
mdeemir <95213547+mdeemir@users.noreply.github.com>
2022-04-15 06:51:45 +0300

    parent class is created

---
+++ src/day39_Recap/cydeoTask/Person.java
@@ -0,0 +1,73 @@
+package day39_Recap.cydeoTask;
+
+public class Person {
+
+    private String name; // koşul eklemek istediğimiz için private yaptık
+    private int age; // generate kısayoldan get ve stter
+    private char gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public Person(String name, int age, char gender) {//constructor ---> genrate constroctor
+        setName(name);//koşula uygun verileri alamak istideğim için setName kullandım
+        setAge(age);
+        setGender(gender);
+    }
+
+    public void eat(){
+        System.out.println(name+" is eating");
+    }
+
+    public void drink(){
+        System.out.println(name+" is drinking");
+    }
+
+    public void sleep(){
+        System.out.println(name+" is sleeping");
+    }
+
+
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                '}';
+    }
+
+}
+
+/*
+1. Create a class named Person:
+            Variables:
+                name, age, gender
+            Encapsulate all the fields
+            Add a constructor to set all the fields
+            Methods:
+                eat()
+                drink()
+                sleep()
+                toString()
+ */
