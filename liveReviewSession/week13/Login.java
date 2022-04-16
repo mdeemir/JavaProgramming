3fcca7c80f7067696ece7390301673daa3d70819
mdeemir <95213547+mdeemir@users.noreply.github.com>
2022-04-16 14:12:28 +0300

    final class

---
+++ liveReviewSession/week13/Login.java
@@ -0,0 +1,17 @@
+package week13;
+
+public class Login {
+    public static void main(String[] args) {
+
+        Credentials userOne = new Credentials();
+/*        System.out.println("Username at the system for userOne is: "+userOne.userName);
+        System.out.println("Password at the system for userOne is: "+userOne.passWord);
+        userOne.userName= "differentName";---> değiştirebiliryorusn istediğin gibi private olmazsa
+        userOne.passWord= "differentPassword";---> değiştirebiliryorusn istediğin gibi private olmazsa
+        System.out.println("Username at the system for userOne is: "+userOne.userName);
+        System.out.println("Password at the system for userOne is: "+userOne.passWord);*/
+
+
+    }
+
+}
