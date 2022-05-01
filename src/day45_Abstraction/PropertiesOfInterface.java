6dcc3b2963aec9d87885d4ec5e296d51e9492949
mdeemir <95213547+mdeemir@users.noreply.github.com>
2022-05-01 19:07:41 +0300

    day 45 interface started

--- src/day45_Abstraction/PropertiesOfInterface.java
+++ src/day45_Abstraction/PropertiesOfInterface.java
@@ -22,14 +22,25 @@ public interface PropertiesOfInterface {
         System.out.println("Instance method");
     }
      */
-
-    static void method2(){
+// interface de kullannabildiğin 3 metod türü
+    static void method2(){ // PropertiesOfInterface ismiyle cağırabilirsin
         System.out.println("Static method");
     }
 
     void method3();
 
     default void method4(){
+        /* java 8 den sonra eklendi. eğer interface içinde normal class taki gibi instance bir metod ve  kod blogu
+        ile bi metod oluşturmak istiyorsan interface de bunu default anahtar kelimesi ile yapabiliyorsun sadece
+
+          void method3(); bu abstract metod ile blok cağıramazsın
+
+          blok yazmanın bize sağladığı kolaylık interfacede bi kere yazdığın bi metodu sub claslarda bunu
+          ine aynen yazacaksan bir kere yazmanı sağlıyor. diğer sub claslara bu inheritance ile aktarılıyor
+
+yani interface da instance metod kullanamdığımız içi javaya default metod eklenmiş ve interface de instan gibi
+kullanabiliyorux
+         */
         System.out.println("Default method");
     }
 
@@ -37,16 +48,17 @@ public interface PropertiesOfInterface {
 
 class Test implements PropertiesOfInterface{
 
-    @Override
+    @Override  // abstract metodun gövdesi olmadığı için mecbur implement eklemelisin yoksa hata verir
     public void method3() {
 
     }
 
     public static void main(String[] args) {
 
-        new Test().method4();
+        new Test().method4();  //default olan metodu class ismi ile cagırabildim
 
     }
 
 
 }
+
